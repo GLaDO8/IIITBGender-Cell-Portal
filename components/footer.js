ecf8c9fbaf2b98799e46c9c829ee85322cf596a9
shreyas gupta <shreyas.charming@gmail.com>
2021-02-05 12:21:47 +0530

    updated footer

--- components/footer.js
+++ components/footer.js
@@ -1,11 +1,15 @@
 export default function Footer() {
   return (
     <>
-      <div className="container mx-auto max-w-lg md:max-w-3xl lg:max-w-4xl mb-16 text-xs">
+      <div className="bottom-0 w-full mx-auto max-w-lg md:max-w-3xl lg:max-w-4xl mb-16 text-xs">
         <p>
           Gender Cell Portal is a committee of IIIT Bangalore. The code for this
           website is{" "}
-          <a href="" rel="_blank">
+          <a
+            href="https://github.com/GLaDO8/IIITBGender-Cell-Portal"
+            rel="_blank"
+            className="cursor-pointer link-styling"
+          >
             open-source
           </a>
           .
