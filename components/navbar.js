67cefcb44214a20eeedb3325f23db5097d05316e
shreyas gupta <shreyas.charming@gmail.com>
2021-02-05 11:45:53 +0530

    changed navbar padding

--- components/navbar.js
+++ components/navbar.js
@@ -4,7 +4,7 @@ import GCPLogo from "./../public/gender-cell-logo.svg";
 const Navbar = (props) => (
   <>
     <div className="container mx-auto max-w-lg md:max-w-3xl lg:max-w-4xl">
-      <nav className="flex justify-between mt-8 z-10 mb-2 md:mb-4 lg:mb-6">
+      <nav className="flex justify-between mt-8 z-10 mb-6 md:mb-8 lg:mb-12">
         <div>
           <Link href="/">
             <GCPLogo className="h-24 w-24 md:h-24 lg:h-36 lg:w-36 cursor-pointer" />
