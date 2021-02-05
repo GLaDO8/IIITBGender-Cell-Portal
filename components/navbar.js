6311a77e633d241d50b9258c95d85c3180cd89e1
shreyas gupta <shreyas.charming@gmail.com>
2021-02-05 10:50:50 +0530

    added link to logo

--- components/navbar.js
+++ components/navbar.js
@@ -1,11 +1,14 @@
 import NavButton from "./../components/navbutton";
+import Link from "next/link";
 import GCPLogo from "./../public/gender-cell-logo.svg";
 const Navbar = (props) => (
   <>
     <div className="container mx-auto max-w-lg md:max-w-3xl lg:max-w-4xl">
       <nav className="flex justify-between mt-8 z-10 mb-2 md:mb-4 lg:mb-6">
         <div>
-          <GCPLogo className="h-24 w-24 md:h-24 lg:h-36 lg:w-36" />
+          <Link href="/">
+            <GCPLogo className="h-24 w-24 md:h-24 lg:h-36 lg:w-36 cursor-pointer" />
+          </Link>
         </div>
         <div className="flex flex-row items-center ml-16" id="nav-content">
           {props.navButtons.map((button) => (
