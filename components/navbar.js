6c58b09c4002afca699bfba3e694f89494e84a6f
shreyas gupta <shreyas.charming@gmail.com>
2021-02-05 08:06:38 +0530

    fixed navbar consistency

--- components/navbar.js
+++ components/navbar.js
@@ -5,9 +5,9 @@ const Navbar = (props) => (
     <div className="container mx-auto max-w-3xl">
       <nav className="flex justify-between mt-8 z-10 mb-2 md:mb-4 lg:mb-6">
         <div>
-          <GCPLogo className="h-16 md:24 lg:h-36" />
+          <GCPLogo className="h-12 w-12 md:h-18 lg:h-36 lg:w-36" />
         </div>
-        <div className="flex flex-row items-left ml-16" id="nav-content">
+        <div className="flex flex-row items-center ml-16" id="nav-content">
           {props.navButtons.map((button) => (
             <NavButton
               key={button.path}
