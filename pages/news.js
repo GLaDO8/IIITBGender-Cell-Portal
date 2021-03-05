cba50689f7cb3900a05a0f3ed85d3cd387fbcac2
shreyas gupta <shreyas.charming@gmail.com>
2021-03-05 17:06:33 +0530

    added news section

---
+++ pages/news.js
@@ -0,0 +1,109 @@
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+import Layout from "./../components/layout";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+
+// const breakpointColumnsObj = {
+//   default: 2,
+//   1400: 2,
+//   1100: 2,
+//   800: 1,
+// };
+
+export default function News() {
+  return (
+    <>
+      <Layout>
+        <Head>
+          <title>News</title>
+        </Head>
+        <div className="mb-16">
+          <h1 className="title-style mb-16">Latest News</h1>
+          <h2 className="subtitle-style">Women's Day Celebrations, 2020</h2>
+          <div>
+            <div className="mb-2">
+              <Image
+                src="/images/2020/DSC_0208.jpg"
+                alt=""
+                width="1149"
+                height="610"
+                Layout="responsive"
+                className="rounded-lg overflow-hidden"
+              />
+            </div>
+
+            <ResponsiveMasonry
+              columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 2 }}
+            >
+              <Masonry gutter="12px">
+                <Image
+                  src="/images/2020/DSC_0176.jpg"
+                  alt=""
+                  width="852"
+                  height="723"
+                  Layout="responsive"
+                  className="rounded-lg overflow-hidden"
+                />
+                <Image
+                  src="/images/2020/DSC_0151.jpg"
+                  alt=""
+                  width="1040"
+                  height="717"
+                  Layout="responsive"
+                  className="rounded-lg overflow-hidden"
+                />
+                <Image
+                  src="/images/2020/DSC_0183.jpg"
+                  alt=""
+                  width="1202"
+                  height="648"
+                  Layout="responsive"
+                  className="rounded-lg overflow-hidden"
+                />
+                <Image
+                  src="/images/2020/DSC_0192.jpg"
+                  alt=""
+                  width="1200"
+                  height="640"
+                  Layout="responsive"
+                  className="rounded-lg overflow-hidden"
+                />
+              </Masonry>
+            </ResponsiveMasonry>
+          </div>
+
+          <div>
+            <h2 className="subtitle-style mt-16">
+              Women's Day Celebrations, 2018
+            </h2>
+            <div className="mb-2">
+              <Image
+                src="/images/2018/MIME-act-2018-1.jpg"
+                alt=""
+                width="1024"
+                height="681"
+                Layout="responsive"
+                className="rounded-lg overflow-hidden"
+              />
+            </div>
+            <ResponsiveMasonry
+              columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 2 }}
+            >
+              <Masonry gutter="12px">
+                <Image
+                  src="/images/2018/MIME-act-2018-2.jpg"
+                  alt=""
+                  width="1024"
+                  height="681"
+                  Layout="responsive"
+                  className="rounded-lg overflow-hidden"
+                />
+              </Masonry>
+            </ResponsiveMasonry>
+          </div>
+        </div>
+      </Layout>
+    </>
+  );
+}
